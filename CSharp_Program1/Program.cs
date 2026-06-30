d2b4c504869ede33b07098fe098cd6c22022b688
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-06-30 16:44:36 +0400

    Uncommented all, testing all varaibles, operators, if statements and case statement.

--- CSharp_Program1/Program.cs
+++ CSharp_Program1/Program.cs
@@ -4,70 +4,70 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Please enter your name: ");
-            //string userName = Console.ReadLine();
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
 
-            //Console.WriteLine("Please enter your Age: ");
-            //int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your Age: ");
+            int userAge = int.Parse(Console.ReadLine());
 
-            //Console.WriteLine("Please enter your salary: ");
-            //float sal = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your salary: ");
+            float sal = float.Parse(Console.ReadLine());
 
-            //Console.WriteLine("Hello, " + userName + ", Your age is " + userAge + ", and your salary is " + sal);
-
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            //Console.WriteLine("Please enter first number: ");
-            //float firstNo = float.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Please enter second number: ");
-            //float secondNo = float.Parse(Console.ReadLine());
-
-            //float additionResult = firstNo + secondNo;
-            //float subtractResult = firstNo - secondNo;
-            //float multiplicationResult = firstNo * secondNo;
-            //float divisionResult = firstNo / secondNo;
-            //float reminderResult = firstNo % secondNo;
-
-            //Console.WriteLine("Addition Result: " + additionResult);
-            //Console.WriteLine("Subtraction Result: " + subtractResult);
-            //Console.WriteLine("Multiplication Result: " + multiplicationResult);
-            //Console.WriteLine("Division Result: " + divisionResult);
-            //Console.WriteLine("Reminder Result: " + reminderResult);
-
-            //bool resutl = firstNo > secondNo;
-
-            //Console.WriteLine("Is first number greater than second number? " + resutl);
+            Console.WriteLine("Hello, " + userName + ", Your age is " + userAge + ", and your salary is " + sal);
 
             ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            //Console.WriteLine("Enter your degree: ");
-            //float degree = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter first number: ");
+            float firstNo = float.Parse(Console.ReadLine());
 
-            //if (degree >= 0 && degree < 50)
-            //{
-            //    Console.WriteLine("You failed");
-            //}
-            //else if (degree >= 50 && degree < 60)
-            //{
-            //    Console.WriteLine("You passed with grade D");
-            //}
-            //else if (degree >= 60 && degree < 70)
-            //{
-            //    Console.WriteLine("You passed with grade C");
-            //}
-            //else if (degree >= 70 && degree < 80)
-            //{
-            //    Console.WriteLine("You passed with grade B");
-            //}
-            //else if (degree >= 80 && degree >= 90)
-            //{
-            //    Console.WriteLine("You passed with grade A");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Invalid degree entered");
-            //}
+            Console.WriteLine("Please enter second number: ");
+            float secondNo = float.Parse(Console.ReadLine());
+
+            float additionResult = firstNo + secondNo;
+            float subtractResult = firstNo - secondNo;
+            float multiplicationResult = firstNo * secondNo;
+            float divisionResult = firstNo / secondNo;
+            float reminderResult = firstNo % secondNo;
+
+            Console.WriteLine("Addition Result: " + additionResult);
+            Console.WriteLine("Subtraction Result: " + subtractResult);
+            Console.WriteLine("Multiplication Result: " + multiplicationResult);
+            Console.WriteLine("Division Result: " + divisionResult);
+            Console.WriteLine("Reminder Result: " + reminderResult);
+
+            bool resutl = firstNo > secondNo;
+
+            Console.WriteLine("Is first number greater than second number? " + resutl);
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            Console.WriteLine("Enter your degree: ");
+            float degree = float.Parse(Console.ReadLine());
+
+            if (degree >= 0 && degree < 50)
+            {
+                Console.WriteLine("You failed");
+            }
+            else if (degree >= 50 && degree < 60)
+            {
+                Console.WriteLine("You passed with grade D");
+            }
+            else if (degree >= 60 && degree < 70)
+            {
+                Console.WriteLine("You passed with grade C");
+            }
+            else if (degree >= 70 && degree < 80)
+            {
+                Console.WriteLine("You passed with grade B");
+            }
+            else if (degree >= 80 && degree >= 90)
+            {
+                Console.WriteLine("You passed with grade A");
+            }
+            else
+            {
+                Console.WriteLine("Invalid degree entered");
+            }
 
             ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             Console.WriteLine("Welcome to main menu: ");
