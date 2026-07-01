218f241152533277c864ff062d027620d02d5d7a
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-01 14:03:08 +0400

    Task 1 - Personal Info Card

---
+++ C#_Fundamentals_Part1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace C__Fundamentals_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Easy - Task 1
+            string name = "Alwaleed";
+            int age = 22;
+            double height = 1.85;
+            bool student = true;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height); 
+            if (student)
+            {
+                Console.WriteLine("You are a student.");
+            }
+            else
+            {
+                Console.WriteLine("You are not a student.");
+            }
+        }
+}
+}
