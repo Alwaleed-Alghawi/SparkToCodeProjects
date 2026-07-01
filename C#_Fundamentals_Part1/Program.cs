78c077541ad186a821be3cfa6e69204ec4f20be1
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-01 20:38:26 +0400

    Completed Task 9 - Day Name Finder

--- C#_Fundamentals_Part1/Program.cs
+++ C#_Fundamentals_Part1/Program.cs
@@ -173,6 +173,45 @@ namespace C__Fundamentals_Part1
                 Console.WriteLine("Loyalty member? : " + loyalty);
                 Console.WriteLine("Final Amount to Pay: " + finalBill);
             }
+
+            //Medium - Task 9 - Day Name Finder
+            Console.WriteLine("Enter a number between 1 and 7: ");
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 4: 
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }       
         }
     }
 }
