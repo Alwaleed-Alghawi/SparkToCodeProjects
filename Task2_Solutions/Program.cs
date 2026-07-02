effc5af10f0bd05df8bb96f2f4964713bcbdfac7
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-02 15:14:32 +0400

    Completed Task 1 - Countdown timer

--- Task2_Solutions/Program.cs
+++ Task2_Solutions/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Easy - Task 1 - Countdown timer 
+            Console.WriteLine("Enter the number to start the countdown:");
+            int countdown = int.Parse(Console.ReadLine());
+
+            for (int i = countdown; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
+
         }
     }
 }
