1745bc63d158b69e1ed2be71e44282e4dfb9137b
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-02 15:37:34 +0400

    Completed Task 2 - Sum of numbers 1 to N

--- Task2_Solutions/Program.cs
+++ Task2_Solutions/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             //Easy - Task 1 - Countdown timer 
-            Console.WriteLine("Enter the number to start the countdown:");
+            Console.WriteLine("Enter the number to start the countdown: ");
             int countdown = int.Parse(Console.ReadLine());
 
             for (int i = countdown; i >= 1; i--)
@@ -14,6 +14,18 @@
             }
             Console.WriteLine("Liftoff!");
 
+
+            //Easy - Task 2 - Sum of numbers 1 to N
+            Console.WriteLine("Enter a positvie whole number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum of numbers from 1 to {number} is : {sum}");
         }
     }
 }
