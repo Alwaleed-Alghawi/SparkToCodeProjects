b9e8cfe7176876070ddfb1e8087cbfb983ec4a97
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-05 18:21:10 +0400

    Completed Task 1 - Absolute Difference

---
+++ Task3_Solutions/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task3_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Easy - Task 1 - Absolute Difference
+            Console.Write("Enter the first number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double difference = Math.Abs(num1 - num2);
+
+            Console.WriteLine("The positive difference is: " + difference);
+
+            
+        }
+    }
+}
