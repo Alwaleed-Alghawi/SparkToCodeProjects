88b8f505857ce2363c67480385d4124031707cfa
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-06 18:39:29 +0400

    Created the solutions file

---
+++ Tas4_Solutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tas4_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
