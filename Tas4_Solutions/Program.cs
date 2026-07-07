e88693adf0d679fa6b9ae601e679258449e84cfc
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-07 12:07:19 +0400

    Completed Task 5 & Task 6

--- Tas4_Solutions/Program.cs
+++ Tas4_Solutions/Program.cs
@@ -32,6 +32,25 @@
             Console.WriteLine("3) Exit");
         }
 
+
+        //Medium - Task 5 - Even or Odd Function
+        static bool IsEven(int number)
+        {
+            return (number % 2 == 0);
+        }
+
+
+        //Medium - Task 6 - Rectangle Area & Perimeter Functions
+        static double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        static double CalculatePerimeter(double length, double width)
+        {
+            return 2 * (length + width);
+        }
+
         static void Main(string[] args)
         {
             //Easy - Task 1 - Personalized Welcome Function Calling
@@ -56,6 +75,34 @@
 
             //Easy - Task 4 - Fixed Menu Display Function Calling
             DisplayMenu();
+
+
+            //Medium - Task 5 - Even or Odd Function Calling
+            Console.Write("Enter a number: ");
+            int userNumber1 = int.Parse(Console.ReadLine());
+            bool result2 = IsEven(userNumber);
+
+            if (result2)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+
+            //Medium - Task 6 - Rectangle Area & Perimeter Functions Calling
+            Console.Write("Enter length: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = CalculateArea(length, width);
+            double perimeter = CalculatePerimeter(length, width);
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perimeter);
         }
     }
 }
