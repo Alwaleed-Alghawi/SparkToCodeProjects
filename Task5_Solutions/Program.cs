2b91f50063efa19109455c8e9fb4b4b264af97e3
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-08 15:04:59 +0400

    Completed Task 2 - Dynamic To-Do List

--- Task5_Solutions/Program.cs
+++ Task5_Solutions/Program.cs
@@ -20,7 +20,29 @@
             }
 
 
-            
+            //Easy - Task 2 - Dynamic To-Do List
+            List<string> tasks = new List<string>();
+            string task;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a task (or type 'exit' to stop): ");
+                task = Console.ReadLine();
+
+                if (task.ToLower() == "exit")
+                {
+                    break;
+
+                }
+                else
+                { tasks.Add(task); }
+            }
+
+            Console.WriteLine("Your To-Do List:");
+            foreach (string t in tasks)
+            {
+                Console.WriteLine("- " + t);
+            }
         }
     }
 }
