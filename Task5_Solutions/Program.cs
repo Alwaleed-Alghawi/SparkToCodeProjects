2adf341c1c39508dfea5db18627c5db272d1881c
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-08 15:00:48 +0400

    Completed Task 1- Fixed Grades Array

---
+++ Task5_Solutions/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task5_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Easy - Task 1 - Fixed Grades Array
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine("Enter Grade: ");
+                int grade = int.Parse(Console.ReadLine());
+                grades[i] = grade;
+            }
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine("Grade: " + grade);
+            }
+
+
+            
+        }
+    }
+}
