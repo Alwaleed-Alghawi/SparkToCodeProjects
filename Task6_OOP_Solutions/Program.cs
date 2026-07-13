7b4f85535905c13069ffca1be2b14c2f936ca7fe
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-13 15:12:28 +0400

    Added Solutions file

---
+++ Task6_OOP_Solutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task6_OOP_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
