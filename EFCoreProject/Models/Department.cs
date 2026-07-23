407294b77bf0ee2c0f52b4fc15f859cbf7094b44
Alwaleed Khalid Alghawi <alwalid570@gmail.com>
2026-07-23 23:10:01 +0400

    Completed all steps of EFCore

---
+++ EFCoreProject/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreProject.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; } //1,2,3
+
+        public int DepartmentNumber  { get; set; } //10,20,30
+
+        public string DepartmentName { get; set; }
+
+    }
+}
